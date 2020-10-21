6e87f50658827385701ec7c4e6005d7390b7a565
cjackson914 <63318622+cjackson914@users.noreply.github.com>
2020-10-20 23:31:27 -0400

    updated moon

--- dev/js/animation.js
+++ dev/js/animation.js
@@ -1,48 +1,46 @@
-// import {gsap} from "gsap";
-// import { Timeline } from "gsap/gsap-core";
-
 import {gsap} from "gsap";
-import {MorphSVGPlugin} from "gsap/MorphSVGPlugin";
-
-gsap.registerPlugin(MorphSVGPlugin);
+// import {MorphSVGPlugin} from "gsap/MorphSVGPlugin";
+// gsap.registerPlugin(MorphSVGPlugin);
 
 export function toggleAnimation(){
-
 
 
 
 var toggle = document.getElementById("toggle-button");
 
 // var buttonTl = new TimelineMax({paused:true});
-// buttonTl.to('#background-day', 0.6, {
-//   morphSVG:{shape:'#background-night'}
-//   // ease:Elastic.easeInOut
+// buttonTl.to('#background-day', alpha:0, {
+//   morphSVG:{shape:'#background-night'},
+//   ease:Elastic.easeInOut
 // })
 
 toggle.addEventListener('click',()=>{
   if(toggle.className.includes("active")){
     toggle.className = "toggle-button";
-    // backgroundTL.play();
     buttonTl.reverse();
   } else {
     toggle.className = "toggle-button active";
-    // var buttonTl = new TimelineMax({paused:true});
-    // gsap.to("background-day", {duration:1, alpha:0});
-    // backgroundTL.reverse();
-
+    
     buttonTl.play();
   }  
 });
+
+// var background = backgroundNight.from("#background-night",{alpha:0});
+// background.reverse();
+
+// toggle.onclick = function() {
+//   background.reversed(!background.reversed());
+// }
+
+// toggle.addEventListener('click',()=>{
+//   if(toggle.className.includes("active")){
+//     toggle.className = "background-night";
+//     buttonTl.reverse();
+//   } else {
+//     toggle.className = "background-day";
+    
+//     buttonTl.play();
+//   }  
+// });
+
 }
-
-
-const backgroundTL = gsap.timeline({
-   
-});
-backgroundTL.to("#background-day", {duration: .5, morphSVG: "#background-night"})
-;
-
-export function backgroundAnimation(){
-  return backgroundTL;
-}
-
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,30 +1,12 @@
 import {gsap} from "gsap";
 
-import {toggleAnimation, backgroundAnimation} from "./animation.js";
-toggleAnimation();
-backgroundAnimation();
+import {toggleAnimation} from "./animation.js";
+// import {backgroundAnimation} from "./background.js";
+// toggleAnimation();
+// backgroundNight();
+// backgroundAnimation();
 
 
 const mainTL = gsap.timeline({paused:true});
 
-mainTL.add(toggleAnimation());
-
-
-// var toggle = document.getElementById("toggle-button");
-
-// // var buttonTl = new TimelineMax({paused:true});
-// // buttonTl.to('#cross', 0.6, {
-// //   morphSVG:{shape:'#checkmark'},
-// //   ease:Elastic.easeInOut
-// // })
-
-// toggle.addEventListener('click',()=>{
-//   if(toggle.className.includes("active")){
-//     toggle.className = "toggle-button";
-//     buttonTl.reverse();
-//   } else {
-//     toggle.className = "toggle-button active";
-//     buttonTl.play();
-//   }  
-// });
-
+mainTL.add(toggleAnimation())
