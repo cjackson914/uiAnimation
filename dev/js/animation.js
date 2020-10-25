fff83477ea5645dac354689f5cba302acfc52aa4
cjackson914 <63318622+cjackson914@users.noreply.github.com>
2020-10-24 22:41:56 -0400

    all content fade in and out

--- dev/js/animation.js
+++ dev/js/animation.js
@@ -1,29 +1,36 @@
 import {gsap} from "gsap";
-// import {MorphSVGPlugin} from "gsap/MorphSVGPlugin";
-// gsap.registerPlugin(MorphSVGPlugin);
+import {MorphSVGPlugin} from "gsap/MorphSVGPlugin";
+import {DrawSVGPlugin} from "gsap/DrawSVGPlugin";
+gsap.registerPlugin(MorphSVGPlugin);
+gsap.registerPlugin(DrawSVGPlugin);
+
+const campingTL = gsap.timeline();
 
 export function toggleAnimation(){
 
+  campingTL.to("#sky",{duration:1, fill:"#3a6f84"},"play")
+  .to("#sun",{morphSVG:"#moon", duration: 1, x:192, alpha:0},"play")
+  // .from("#moon",{morphSVG:"#sun", duration: 1, x:-192, alpha:0},"play")
+
+  .to("#ground",{duration: 1, fill:"#29353a"},"play")
+  .to("#lake",{duration: 1, alpha:0},"play")
+  .to("#canoe",{duration: 1, alpha:0},"play")
+  .to("#table",{duration: 1, alpha:0},"play")
+  .to("#path1, #path2",{duration: 1, alpha:0, drawSVG:"100%"},"play")
+  .to("#tree1, #tree2, #tree3",{duration: 1, alpha:0},"play")
+
+  .from("#tent",{duration: 1, alpha:0},"play")
+  .from("#logs",{duration: 1, alpha:0},"play")
+  .from("#fire",{duration: 1, alpha:0},"play")
+  .from("#mainstar",{duration: 1, alpha:0},"play")
+  .from("#shoot1",{duration: 1, alpha:0, drawSVG: "100%"},"play")
+  .from("#shoot2",{duration: 1, alpha:0},"play")
+  .from("#stars",{duration: 1, alpha:0},"play")
+  .from("#tree4, #tree5, #tree6",{duration: 1, alpha:0},"play");
 
 
-var toggle = document.getElementById("toggle-button");
+  return campingTL;
 
-// var buttonTl = new TimelineMax({paused:true});
-// buttonTl.to('#background-day', alpha:0, {
-//   morphSVG:{shape:'#background-night'},
-//   ease:Elastic.easeInOut
-// })
-
-toggle.addEventListener('click',()=>{
-  if(toggle.className.includes("active")){
-    toggle.className = "toggle-button";
-    buttonTl.reverse();
-  } else {
-    toggle.className = "toggle-button active";
-    
-    buttonTl.play();
-  }  
-});
 
 // var background = backgroundNight.from("#background-night",{alpha:0});
 // background.reverse();
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -10,3 +10,34 @@ import {toggleAnimation} from "./animation.js";
 const mainTL = gsap.timeline({paused:true});
 
 mainTL.add(toggleAnimation())
+
+var isNight = false;
+
+
+var toggle = document.getElementById("toggle-button");
+
+// var buttonTl = new TimelineMax({paused:true});
+// buttonTl.to('#background-day', alpha:0, {
+//   morphSVG:{shape:'#background-night'},
+//   ease:Elastic.easeInOut
+// })
+
+toggle.addEventListener('click',()=>{
+  
+
+    
+  //if(toggle.className.includes("active")){
+    if(isNight === false){
+    //toggle.className = "toggle-button";
+    //buttonTl.reverse();
+    mainTL.play();
+    isNight = true;
+  } else {
+    //toggle.className = "toggle-button active";
+    
+    //buttonTl.play();
+
+    mainTL.reverse();
+    isNight = false;
+  }  
+});
