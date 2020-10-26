9c9842ca32154b9b911962ce54fc1d0b4823af0f
cjackson914 <63318622+cjackson914@users.noreply.github.com>
2020-10-25 21:29:24 -0400

    done! (other than drawSVG)

--- dev/js/animation.js
+++ dev/js/animation.js
@@ -10,24 +10,25 @@ export function toggleAnimation(){
 
   campingTL.to("#sky",{duration:1, fill:"#3a6f84"},"play")
   // .to("#sun",{morphSVG:"#moon", duration: 1, x:192},"play")
-  .to("#sun",{morphSVG:{shape:"#moon"}, duration: 1, x:192},"play")
-
+  // .to("#sun",{morphSVG:{shape:"#moon"}, duration: 1, x:50},"play")
+  // .from("#moon",{morphSVG:{shape:"#sun"}, duration: 1, fill:"#EFD9A7"},"play")
+  .to("#sun",{morphSVG:{shape:"#moon"}, duration: 1, x:193, fill:"#D1D3D4"},"play")
+  .from("#mooncraters",{duration: .3, alpha:0})
 
   .to("#ground",{duration: 1, fill:"#29353a"},"play")
   .to("#lake",{duration: 1, alpha:0},"play")
-  .to("#canoe",{duration: 1, alpha:0},"play")
-  .to("#table",{duration: 1, alpha:0},"play")
+  .to("#canoe",{duration: 1, alpha:0, x:15},"play")
+  .to("#table",{duration: 1, alpha:0, scale:.8},"play")
   .to("#path1, #path2",{duration: 1, alpha:0, drawSVG:"100%"},"play")
-  .to("#tree1, #tree2, #tree3",{duration: 1, alpha:0},"play")
+  .to("#tree1, #tree2, #tree3",{duration: 1, alpha:0, y:5},"play")
 
-  .from("#tent",{duration: 1, alpha:0},"play")
-  .from("#logs-2",{duration: 1, alpha:0},"play")
-  .from("#fire",{duration: 1, alpha:0},"play")
-  .from("#mainstar",{duration: 1, alpha:0},"play")
-  .from("#shoot1",{duration: 1, alpha:0, drawSVG: "100%"},"play")
-  .from("#shoot2",{duration: 1, alpha:0},"play")
-  .from("#stars",{duration: 1, alpha:0},"play")
-  .from("#tree4, #tree5, #tree6",{duration: 1, alpha:0},"play");
+  .from("#tent",{duration: 1, alpha:0, scale:1.2},"play")
+  .from("#logs-2",{duration: 1, alpha:0, scale:.8},"play")
+  .from("#fire",{duration: 1, alpha:0, scale:.5, y:5})
+  .from("#main-star",{duration: 1, alpha:0, x:-30, y:15, rotate:15, drawSVG: "100%"},"shooting-star")
+  .from("#shoot1, #shoot2",{duration: 1, alpha:0, x:-30, y:15, rotate:-15, drawSVG: "100%"},"shooting-star")
+  .from("#star-1, #star-2, #star-3, #star-4, #star-5, #star-6",{duration: .5, ease: "none", yoyo:true, scale:.5, alpha:0, stagger:.2},"play")
+  .from("#tree4, #tree5, #tree6",{duration: 1, alpha:0, y:5},"play");
 
 
   return campingTL;
